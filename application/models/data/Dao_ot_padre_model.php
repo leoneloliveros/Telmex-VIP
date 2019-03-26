6dc2740000136879326bc1029658ab16f8842443
unknown <jfchaparro33@misena.edu.co>
2019-03-26 11:35:55 -0500

    archivos de prueba archivos excel de prueba para el issue #1

--- application/models/data/Dao_ot_padre_model.php
+++ application/models/data/Dao_ot_padre_model.php
@@ -598,10 +598,11 @@ class Dao_ot_padre_model extends CI_Model {
                     AND otp1.k_id_user = u.k_id_user
                     AND otp1.n_nombre_cliente NOT IN ('BANCO COLPATRIA RED MULTIBANCA COLPATRIA S.A', 'BANCO DAVIVIENDA S.A', 'SERVIBANCA S.A.')
                     AND otp1.orden_trabajo != 'Caso de Seguimiento'
+                    /* si solo se quiere mostrar otp que tengan oth descomentariar esta seccion
                     AND EXISTS(
                         SELECT nro_ot_onyx FROM ot_hija AS oth1
                         WHERE otp1.k_id_ot_padre = oth1.nro_ot_onyx
-                    )
+                    )*/
 
                 ) AS menor_7,
                 (
@@ -611,10 +612,11 @@ class Dao_ot_padre_model extends CI_Model {
                     AND otp2.k_id_user = u.k_id_user
                     AND otp2.n_nombre_cliente NOT IN ('BANCO COLPATRIA RED MULTIBANCA COLPATRIA S.A', 'BANCO DAVIVIENDA S.A', 'SERVIBANCA S.A.')
                     AND otp2.orden_trabajo != 'Caso de Seguimiento'
+                    /* si solo se quiere mostrar otp que tengan oth descomentariar esta seccion
                     AND EXISTS(
                         SELECT nro_ot_onyx FROM ot_hija AS oth2
                         WHERE otp2.k_id_ot_padre = oth2.nro_ot_onyx
-                    )
+                    )*/
 
                 ) AS entre_8_15,
                 (
@@ -624,10 +626,11 @@ class Dao_ot_padre_model extends CI_Model {
                     AND otp3.k_id_user = u.k_id_user
                     AND otp3.n_nombre_cliente NOT IN ('BANCO COLPATRIA RED MULTIBANCA COLPATRIA S.A', 'BANCO DAVIVIENDA S.A', 'SERVIBANCA S.A.')
                     AND otp3.orden_trabajo != 'Caso de Seguimiento'
+                    /* si solo se quiere mostrar otp que tengan oth descomentariar esta seccion
                     AND EXISTS(
                         SELECT nro_ot_onyx FROM ot_hija AS oth3
                         WHERE otp3.k_id_ot_padre = oth3.nro_ot_onyx
-                    )
+                    )*/
 
                 ) AS entre_16_30,
                 (
@@ -636,10 +639,11 @@ class Dao_ot_padre_model extends CI_Model {
                     AND otp4.k_id_user = u.k_id_user
                     AND otp4.n_nombre_cliente NOT IN ('BANCO COLPATRIA RED MULTIBANCA COLPATRIA S.A', 'BANCO DAVIVIENDA S.A', 'SERVIBANCA S.A.')
                     AND otp4.orden_trabajo != 'Caso de Seguimiento'
+                    /* si solo se quiere mostrar otp que tengan oth descomentariar esta seccion
                     AND EXISTS(
                         SELECT nro_ot_onyx FROM ot_hija AS oth4
                         WHERE otp4.k_id_ot_padre = oth4.nro_ot_onyx
-                    )
+                    )*/
 
                 ) AS mayor_30
             FROM user u
